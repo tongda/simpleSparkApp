0aa6af6837ab95accf7aba582cb1cad670dcfc14
Tong Damon Da <tongda@outlook.com>
2014-11-28 18:03:21 +0800

    add config

--- build.sbt
+++ build.sbt
@@ -6,5 +6,7 @@ scalaVersion := "2.10.4"
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.1.0"
 
+libraryDependencies += "com.typesafe" % "config" % "1.2.1"
+
 resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
     
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,11 +1,15 @@
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "../spark/README.md"
-    val conf = new SparkConf().setAppName("Simple Application")
+    val config = ConfigFactory.load()
+    val logFile = "s3n://tw-data-lab/tongda/simpleSparkApp/README.md"
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .setMaster(config.getString("spark-cluster.master"))
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
